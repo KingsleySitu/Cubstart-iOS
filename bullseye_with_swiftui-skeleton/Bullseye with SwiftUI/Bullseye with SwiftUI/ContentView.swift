90f9c51cc27c4eaf42d3f5be8c09c4e0dc417305
Kingsley Situ <kingsleysitu@berkeley.edu>
2022-10-06 22:38:27 -0700

    HW3 finished

--- bullseye_with_swiftui-skeleton/Bullseye with SwiftUI/Bullseye with SwiftUI/ContentView.swift
+++ bullseye_with_swiftui-skeleton/Bullseye with SwiftUI/Bullseye with SwiftUI/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+    @State private var num: Double = 50
+    @State private var exactOn = false
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    var body: some View {
+        // high score and current level
+        HStack {
+            VStack() {
+                Text("High Score")
+                Text("0")
+            }.padding(.top, 20).padding(.leading, 20)
+            Spacer()
+            VStack() {
+                Text("Current Level")
+                Text("1")
+            }.padding(.top, 20).padding(.trailing, 20)
+        }
+        
+        Spacer()
+        
+        // slider
+        VStack {
+            Text("Move the slider to:").font(.system(size: 30))
+            Text("25").font(.system(size: 30))
+            Slider(value: $num, in: 0...100).padding(.leading, 20).padding(.trailing, 20)
+            Button("check") {}
+        }
+        
+        Spacer()
+        
+        // toggle
+        VStack {
+            Text("Exact Mode?")
+            Toggle("", isOn: $exactOn).labelsHidden()
+        }
     }
 }
